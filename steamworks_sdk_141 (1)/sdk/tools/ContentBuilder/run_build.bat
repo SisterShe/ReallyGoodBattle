28d001f5cf7d2f55cc3b8f2d4c76e1f867c61767
Storm Hughes <31044937+BoxheadPixelart@users.noreply.github.com>
2018-02-11 18:01:50 -0600

    Subtle Menu Changes
    I also updated the Steam SDK nonsense and uploaded a few builds

--- steamworks_sdk_141 (1)/sdk/tools/ContentBuilder/run_build.bat
+++ steamworks_sdk_141 (1)/sdk/tools/ContentBuilder/run_build.bat
@@ -1,1 +1,1 @@
-builder\steamcmd.exe +login undeadstormm Catphonespacepen1 +run_app_build_http ..\scripts\app_build_1000.vdf +quit
+builder\steamcmd.exe +login undeadstormm CatPhoneSpacePen1! +run_app_build_http ..\scripts\app_build_1000.vdf +quit
